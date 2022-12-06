9c61d10a4e6be02193a0d87ce7d6dcc1e41aeac5
abiraltamang <abiral.tamang816@gmail.com>
2022-12-06 12:33:17 +0545

    oop concept

--- oop.py
+++ oop.py
@@ -23,16 +23,59 @@ Online News Portal
         add(), view(), delete()
 
 Online Student Enrollment
+    Course
+        title duration fee shift affiliation department status
+        add(), view() update() delte()
+
+    Enrollments
+        student course  
+
+
 
 Online Job Portal
+    User
+        user, mobile, email, username, password
+        register(), update(), view(), login(), delete
     Job
         title detail requirement salary company vacancy_no category
         register(), update(), view(), login(), delete()
+    Application
+        user job
 
 Online shopping (E-commerce)
+    User
+        name, mobile, username, password, image, cv,type, status
+        register(), update(), view(), login(), delete()
     Product
         name price details stock featuresn category brand types
         add(),view(), update(), delete()
 
 '''
+
+class User:
+    full_name= ''
+    mobile= ''
+    email = ''
+    address = ''
+    password = ''
+    type = ''
+
+    def register(self):
+        pass
+    
+    @staticmethod
+    def view():
+        pass
+    
+    def login():
+        pass
+
+    def search():
+        pass
+
+    def update():
+        pass
+
+    def delete():
+        pass 
 
