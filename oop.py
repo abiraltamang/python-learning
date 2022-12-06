63926703bf55b6ea3a3868530bfe38dd3af4eb44
abiraltamang <abiral.tamang816@gmail.com>
2022-12-06 12:26:31 +0545

    ..

---
+++ oop.py
@@ -0,0 +1,38 @@
+'''
+OOP
+----
+
+FEATURES OF OOP
+1. Enscapsulation
+2. Inheritance
+3. Polymorphism
+    a. Method overloading
+    b. Method overriding
+4. Abstraction(Interface)
+
+''''''
+Online News Portal
+    User
+        name, mobile, email, username, password
+        register(), update(), view(), login(), delete()
+    News
+        topic, date, details, authors, category
+        add(),view(), update(), delete()
+    comment
+        fullname, email, comments, 
+        add(), view(), delete()
+
+Online Student Enrollment
+
+Online Job Portal
+    Job
+        title detail requirement salary company vacancy_no category
+        register(), update(), view(), login(), delete()
+
+Online shopping (E-commerce)
+    Product
+        name price details stock featuresn category brand types
+        add(),view(), update(), delete()
+
+'''
+
