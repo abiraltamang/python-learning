bf7f271d1a4f9ee79e62643ed30858b344b0c3b5
abiraltamang <abiral.tamang816@gmail.com>
2023-01-04 14:12:18 +0545

    js included

---
+++ frontend/day2/app.js
@@ -0,0 +1,23 @@
+// selectors
+
+
+/**
+ * 
+    Js can change
+    1. content
+    2. attribute
+    3. style
+    4. structure
+
+
+
+
+ */
+
+    function myFunction(){
+        document.getElementById('test').innerHTML = "Welcome";
+        // document.getElementById('text').setAttribute
+        document.getElementById('text').align  = "center";
+        document.getElementById('text').style.color ="red";
+        // document.getElementById('test')
+    }
